2077b221ad1b747ef994f339f2aa401244f93b74
fagner ferreira <fagnerrc12@gmail.com>
2024-11-27 13:49:04 -0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from http import HTTPStatus
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
+import pandas as pd
+import os
 
 app = FastAPI()
+path = "dados.csv"
+data = pd.DataFrame(
+    data={
+        "id": [],
+        "titulo": [],
+        "autor": [],
+        "ano": [],
+        "genero": [],
+        "editora": [],
+    }
+)
+if not os.path.exists(path):
+    data.to_csv(path)
+data = pd.read_csv(path)
 
 
 class Livro(BaseModel):

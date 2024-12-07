1e5a415b0f67ff0580f89782791d76d7f6e2a798
Fagner Ferreira <84201632+fagner02@users.noreply.github.com>
2024-12-06 21:21:05 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-with open('dados.csv', mode='r', newline='', encoding='utf-8') as file:
-            reader = pd.read_csv(file)
-            teste = reader [(reader['id'] == 1) & (reader['titulo'] == 'Culture debate')]
-            print(teste)

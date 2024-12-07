1cf518cdec3fe3f53b680f497af731837599c04d
fagner ferreira <fagnerrc12@gmail.com>
2024-12-06 21:00:28 -0300

    update

--- front/src/BookFetch.js
+++ front/src/BookFetch.js
@@ -1,38 +1,31 @@
-// @ts-nocheck
-import { createElement } from "react";
-
 const PersonFetch = {
-  // @ts-ignore
   getAll: /**@return {Promise<import("./types").Livro[]>}*/ async () => {
-    // @ts-ignore
     return await fetch(process.env.REACT_APP_API + "livros")
       .then(async (response) => {
         let temp = [];
         temp = await response.json();
         return temp;
       })
-      .catch((error) => {
+      .catch(() => {
         return [];
       });
   },
-  // @ts-ignore
+
   getById: /**@return {Promise<import("./types").Livro?>}*/ async (
     /**@type {number}*/ id
   ) => {
-    // @ts-ignore
     var result = await fetch(process.env.REACT_APP_API + "livros/" + id)
       .then(async (response) => {
         let temp = {};
         temp = await response.json();
         return /**@type {import("./types").Livro}*/ (temp);
       })
-      .catch((error) => {
+      .catch(() => {
         return null;
       });
     return result;
   },
   post: (/**@type {import("./types.js").Livro}*/ data) => {
-    // @ts-ignore
     return fetch(process.env.REACT_APP_API + "livros", {
       body: JSON.stringify(data),
       method: "POST",
@@ -41,9 +34,8 @@ const PersonFetch = {
       },
     });
   },
-  // @ts-ignore
+
   delete: async (/**@type {number[]}*/ ids) => {
-    // @ts-ignore
     await fetch(process.env.REACT_APP_API + "livros", {
       method: "DELETE",
       body: JSON.stringify({ ids: ids }),
@@ -53,9 +45,7 @@ const PersonFetch = {
     });
   },
 
-  // @ts-ignore
   deleteById: /**@param {number} id*/ async (id) => {
-    // @ts-ignore
     await fetch(`${process.env.REACT_APP_API}livros/${id}`, {
       method: "DELETE",
     });
@@ -65,9 +55,7 @@ const PersonFetch = {
      * @param {import("./types.js").Livro} data
      * @param {number} id
      */
-    // @ts-ignore
     async (data, id) => {
-      // @ts-ignore
       await fetch(`${process.env.REACT_APP_API}livros/${id}`, {
         method: "PUT",
         body: JSON.stringify(data),
@@ -91,13 +79,20 @@ const PersonFetch = {
     download.setAttribute("download", "dados.zip");
     return download.click();
   },
-  filtrarLivros: async (id, titulo, autor, genero, editora, ano) => {
-    let query = id == "" ? "" : `id=${id}&`;
-    query += titulo == "" ? "" : `titulo=${titulo}&`;
-    query += autor == "" ? "" : `autor=${autor}&`;
-    query += genero == "" ? "" : `genero=${genero}&`;
-    query += editora == "" ? "" : `editora=${editora}&`;
-    query += ano == "" ? "" : `ano=${ano}&`;
+  filtrarLivros: async (
+    /** @type {string} */ id,
+    /** @type {string} */ titulo,
+    /** @type {string} */ autor,
+    /** @type {string} */ genero,
+    /** @type {string} */ editora,
+    /** @type {string} */ ano
+  ) => {
+    let query = id === "" ? "" : `id=${id}&`;
+    query += titulo === "" ? "" : `titulo=${titulo}&`;
+    query += autor === "" ? "" : `autor=${autor}&`;
+    query += genero === "" ? "" : `genero=${genero}&`;
+    query += editora === "" ? "" : `editora=${editora}&`;
+    query += ano === "" ? "" : `ano=${ano}&`;
     const livros = await fetch(
       `${process.env.REACT_APP_API}livros/filtrar?${query}`,
       {
